724da531c47b0ca839f6f305628ae6065ba44087
Arthur CUVILLON <arthurcuvillon691@gmail.com>
2022-12-13 21:41:10 +0100

    top menu c

--- resources/views/layouts/topMenuC.blade.php
+++ resources/views/layouts/topMenuC.blade.php
@@ -8,40 +8,14 @@
     <ul class="nav-menu">
       <li class="nav-item">
         <div class="toggleconcept">
-          <a href="#" class="nav-link">Concept</a>
-          <div class="concept">
-            <p>La nuit MMI regroupe la communauté des étudiants, des anciens étudiants et des professeurs de la formation autour d’un événement haut en couleurs.</p>  
-            <p>4 salles libre à la découverte exposeront les projets des actuels et anciens étudiants sur volontariat. </p>
-            <p>L'occasion révée pour montrer vos talents.</p>
-            <section class="buttons">
-              <a id="button2">Fermer</a>
-            </section>
-          </div>
+          <a href="/logout" class="nav-link">Déconnecter</a>
         </div>
       </li>
       <li class="nav-item">
         <div class="toggleprogramme">
-          <a href="#" class="nav-link">Programme</a>
-          <div class="programme">
-          <p>19h30 Amphitéatre MMI: début de la soirée.</p>  
-          <p>20h00 salles X: Ouverture de l'exposition des travaux pendant toute la soirée et accès au buffet.</p>
-          <p>22h00 Amphitéatre MMI: projection d'un court-métrage étudiant.</p>
-          <section class="buttons">
-            <a id="button2">Fermer</a>
-            
-          </section>
-        </div>
+          <a href="/createur/{{$id}}" class="nav-link">Profil</a>
         </div>
       </li>
-
-      <li class="nav-item">
-        <a href="/login/">
-        <i class='bx bxs-user'></i>
-      </a>
-      </li>
-      <!-- <li class="nav-item">
-        <a href="#" class="nav-link">FAQ</a>
-      </li> -->
     </ul>
 
     <div class="hamburger">
