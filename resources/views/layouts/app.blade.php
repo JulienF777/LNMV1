4649a2e6a2873f283be2baf9b69d2b9e12e480a0
JulienF777 <juju56260@live.fr>
2022-12-13 20:33:47 +0100

    ajout du petit icone d'onglet dans les layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <title>La nuit MMI</title>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="images/nuitmmisite.png" href="images/nuitmmisite.png" />
+
     <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dosis:wght@200;300;400;500&display=swap" rel="stylesheet">
