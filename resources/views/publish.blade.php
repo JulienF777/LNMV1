a84c2bd56f870da35e8109d15c701b3492545a34
JulienF777 <juju56260@live.fr>
2022-12-13 23:02:39 +0100

    ajout d'une description pour le push des projets

--- resources/views/publish.blade.php
+++ resources/views/publish.blade.php
@@ -8,18 +8,24 @@
   <div class="publish">
   <h3>Envie de faire partie de La Nuit MMI ?</h3>
   <span class="callToAction">Partagez Votre Projet</span>
+  <div class="explicatif">
+    <div class="ex1">
+    <p>Indiquez un nom puis chargez une miniature / une capture du projet. Indiquez un lien pour pouvoir le consulter ou un lien vers un site personnel, un profil LinkedIn, Awwwards... </p>
+    <p>Si vous ne souhaitez pas que votre projet soit lié à votre compte La nuit MMI, vous pouvez cocher "Poster anonymement"</p>
+    </div>
+  </div>
   
   <form action='/publishT' method='POST' enctype="multipart/form-data">
     <input type='text' name='nom' placeholder='Nom de votre Projet' class="champsform">
     @csrf
     <label class="file"><i class='bx bx-upload'></i>Charger une image<input type="file" id="file" name="img" accept="image/*"></label>
     <input class="champsform" type='number' name='anneeReal' placeholder='Année de Réalisation de votre Projet' min="1990" max="2023" >
-    <input class="champsform" type='text' name='lien' placeholder='Lien vers votre projet (obligatoire si web ou video)'>
+    <input class="champsform" type='text' name='lien' placeholder='Lien vers votre projet ou réseaux' required>
     <select name="genre" class="select" required>
       <option value="-1">--Le genre de votre projet--</option>
       <option value="web">Web</option>
-      <option value="videos">Videos</option>
-      <option value="images">Images</option>
+      <option value="video">Video</option>
+      <option value="image">Image</option>
     </select>
     <label class="anonyme" for="anonyme"><input type="checkbox" id="anonyme" name="anonyme" value="anonyme"> Poster anonymement</label>
     <input type='submit' name='publish' value='Publier'>
