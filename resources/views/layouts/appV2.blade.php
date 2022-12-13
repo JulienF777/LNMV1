9d118a06af1d11a97d6012a04e54c03a7a178b69
Arthur CUVILLON <arthurcuvillon691@gmail.com>
2022-12-13 23:54:31 +0100

    icon

--- resources/views/layouts/appV2.blade.php
+++ resources/views/layouts/appV2.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="images/nuitmmisite.png" href="images/nuitmmisite.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Nuit MMI</title>
     <link href="/css/normalize.css" rel="stylesheet">
