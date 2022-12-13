b19a93804ead1bdfd8d3dd23731705fb8cc48154
JulienF777 <juju56260@live.fr>
2022-12-13 21:14:28 +0100

    modifs avant upload

--- resources/views/layouts/topj.blade.php
+++ resources/views/layouts/topj.blade.php
@@ -3,9 +3,7 @@
 <head>
     <title>La nuit MMI</title>
     <meta charset="UTF-8">
-
     <link rel="shortcut icon" type="images/nuitmmisite.png" href="images/nuitmmisite.png" />
-
     <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dosis:wght@200;300;400;500&display=swap" rel="stylesheet">
@@ -19,12 +17,13 @@
 <body>
 
 @section('topMenu')
-  @include('layouts.topMenu')
+  @include('layouts.topMenuC')
 @show
 
+  <section class="content">
     @yield('content')
+  </section>
 
 </body>
-
 <!-- <script src="../main.js"></script>  old thing-->
 </html>
